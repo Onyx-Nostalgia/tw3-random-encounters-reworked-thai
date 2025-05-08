d45efec5baf06aba888f8198610af8b685db8238
Oynx Nostalgia <nostalgia.onyx@gmail.com>
2025-05-08 18:08:55 +0700

    ♻️ refactor: 🔄 update paths for string encoding in batch scripts

--- scripts/encode-csv-strings.bat
+++ scripts/encode-csv-strings.bat
@@ -2,12 +2,14 @@ call "%~dp0variables.cmd"
 
 REM Clean previous final .w3strings outputs from modPath
 if exist "%modPath%\en.w3strings" del /Q "%modPath%\en.w3strings"
-if exist "%modPath%\tr.w3strings" del /Q "%modPath%\tr.w3strings"
+if exist "%modTHPath%\tr.w3strings" del /Q "%modTHPath%\tr.w3strings"
 
 "%modkitpath%\w3strings.exe" --encode "%modPath%\en.w3strings.csv" --id-space 5018
-"%modkitpath%\w3strings.exe" --encode "%modPath%\tr.w3strings.csv" --id-space 5018
+"%modkitpath%\w3strings.exe" --encode "%modTHPath%\tr.w3strings.csv" --id-space 5018
 
-del "%modPath%\*.ws" REM This still deletes from Current Working Directory. Adjust if .ws files are in modPath.
+REM This still deletes from Current Working Directory. Adjust if .ws files are in modPath.
+del "%modPath%\*.ws"
+del "%modTHPath%\*.ws"
 
 rename "%modPath%\en.w3strings.csv.w3strings" en.w3strings
-rename "%modPath%\tr.w3strings.csv.w3strings" tr.w3strings
+rename "%modTHPath%\tr.w3strings.csv.w3strings" tr.w3strings
--- scripts/variables.cmd
+++ scripts/variables.cmd
@@ -1,4 +1,5 @@
 @echo off
 
-set modPath=%~dp0..\mods\mod0RandomEncountersReworked_TH\content
+set modTHPath=%~dp0..\mods\mod0RandomEncountersReworked_TH\content
+set modPath=%~dp0..\mods\modRandomEncountersReworked\content
 set modKitPath=D:\witcher_3_mod_tools\w3strings_encoder
