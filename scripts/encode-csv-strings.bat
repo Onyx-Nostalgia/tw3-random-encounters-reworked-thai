ac0a47ea38a1a31b199c1cb421cd3f00508c9ab7
Oynx Nostalgia <nostalgia.onyx@gmail.com>
2025-05-08 19:11:45 +0700

    🌟 feat: add mod thai_full and thai (en menu)

--- scripts/encode-csv-strings.bat
+++ scripts/encode-csv-strings.bat
@@ -2,14 +2,18 @@ call "%~dp0variables.cmd"
 
 REM Clean previous final .w3strings outputs from modPath
 if exist "%modPath%\en.w3strings" del /Q "%modPath%\en.w3strings"
-if exist "%modTHPath%\tr.w3strings" del /Q "%modTHPath%\tr.w3strings"
+if exist "%modTHfullPath%\tr.w3strings" del /Q "%modTHfullPath%\tr.w3strings"
+if exist "%modENMenuPath%\tr.w3strings" del /Q "%modENMenuPath%\tr.w3strings"
 
 "%modkitpath%\w3strings.exe" --encode "%modPath%\en.w3strings.csv" --id-space 5018
-"%modkitpath%\w3strings.exe" --encode "%modTHPath%\tr.w3strings.csv" --id-space 5018
+"%modkitpath%\w3strings.exe" --encode "%modTHfullPath%\tr.w3strings.csv" --id-space 5018
+"%modkitpath%\w3strings.exe" --encode "%modENMenuPath%\tr.w3strings.csv" --id-space 5018
 
 REM This still deletes from Current Working Directory. Adjust if .ws files are in modPath.
 del "%modPath%\*.ws"
-del "%modTHPath%\*.ws"
+del "%modTHfullPath%\*.ws"
+del "%modENMenuPath%\*.ws"
 
 rename "%modPath%\en.w3strings.csv.w3strings" en.w3strings
-rename "%modTHPath%\tr.w3strings.csv.w3strings" tr.w3strings
+rename "%modTHfullPath%\tr.w3strings.csv.w3strings" tr.w3strings
+rename "%modENMenuPath%\tr.w3strings.csv.w3strings" tr.w3strings
--- scripts/variables.cmd
+++ scripts/variables.cmd
@@ -1,5 +1,6 @@
 @echo off
 
-set modTHPath=%~dp0..\mods\mod0RandomEncountersReworked_TH\content
+set modTHfullPath=%~dp0..\mods\mod0RandomEncountersReworked_TH_full\content
+set modENMenuPath=%~dp0..\mods\mod0RandomEncountersReworked_TH_(en_menu)\content
 set modPath=%~dp0..\mods\modRandomEncountersReworked\content
 set modKitPath=D:\witcher_3_mod_tools\w3strings_encoder
